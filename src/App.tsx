422bd3f1ebbd5957d3bc2269dcadc417097f0cd2
Gared1981 <armeentac@gmail.com>
2025-08-08 10:46:31 -0700

    Create package.json and basic Terrapesca Fleet App structure

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Terrapesca Fleet Management</h1>
+        <p>Fleet management system for Terrapesca operations</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
